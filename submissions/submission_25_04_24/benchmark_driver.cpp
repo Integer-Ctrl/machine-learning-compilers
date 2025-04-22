dfff2da58ffaeffc440a02fa24041efc4f15b270
RivinHD <58261670+RivinHD@users.noreply.github.com>
2025-04-22 18:03:23 +0000

    refactor: more detailed benchmarks prints

--- submissions/submission_25_04_24/benchmark_driver.cpp
+++ submissions/submission_25_04_24/benchmark_driver.cpp
@@ -35,8 +35,8 @@ int main()
     const uint64_t run_instructions_throughput_add = throughput_add(repetitions) * repetitions;
     const auto end_throughput_add = std::chrono::high_resolution_clock::now();
     const std::chrono::duration<double, std::milli> diff_throughput_add = end_throughput_add - start_throughput_add;
-    std::cout << "Executed " << run_instructions_throughput_add << " in " << diff_throughput_add.count() << " milliseconds." << std::endl
-              << "Resulting in " << run_instructions_throughput_add / diff_throughput_add.count() * 1000 << " Instructions per Second!"
+    std::cout << "Executed " << run_instructions_throughput_add << " \"Add\" Instructions in " << diff_throughput_add.count() << " milliseconds." << std::endl
+              << "Resulting in a Throughput of " << run_instructions_throughput_add / diff_throughput_add.count() * 1000 << " Instructions per Second!"
               << std::endl
               << std::endl;
 
@@ -45,8 +45,8 @@ int main()
     const uint64_t run_instructions_throughput_mul = throughput_mul(repetitions) * repetitions;
     const auto end_throughput_mul = std::chrono::high_resolution_clock::now();
     const std::chrono::duration<double, std::milli> diff_throughput_mul = end_throughput_mul - start_throughput_mul;
-    std::cout << "Executed " << run_instructions_throughput_mul << " in " << diff_throughput_mul.count() << " milliseconds." << std::endl
-              << "Resulting in " << run_instructions_throughput_mul / diff_throughput_mul.count() * 1000 << " Instructions per Second!"
+    std::cout << "Executed " << run_instructions_throughput_mul << " \"Mul\" Instructions in " << diff_throughput_mul.count() << " milliseconds." << std::endl
+              << "Resulting in a Throughput of " << run_instructions_throughput_mul / diff_throughput_mul.count() * 1000 << " Instructions per Second!"
               << std::endl
               << std::endl;
 
@@ -55,8 +55,8 @@ int main()
     const uint64_t run_instructions_latency_add = latency_add(repetitions) * repetitions;
     const auto end_latency_add = std::chrono::high_resolution_clock::now();
     const std::chrono::duration<double, std::milli> diff_latency_add = end_latency_add - start_latency_add;
-    std::cout << "Executed " << run_instructions_latency_add << " in " << diff_latency_add.count() << " milliseconds." << std::endl
-              << "Resulting in " << run_instructions_latency_add / diff_latency_add.count() * 1000 << " Instructions per Second!"
+    std::cout << "Executed " << run_instructions_latency_add << " \"Add\" Instructions in " << diff_latency_add.count() << " milliseconds on a single Unit." << std::endl
+              << "Resulting in a Throughput of " << run_instructions_latency_add / diff_latency_add.count() * 1000 << " Instructions per Second!"
               << std::endl
               << std::endl;
 
@@ -65,8 +65,8 @@ int main()
     const uint64_t run_instructions_latency_mul = latency_mul(repetitions) * repetitions;
     const auto end_latency_mul = std::chrono::high_resolution_clock::now();
     const std::chrono::duration<double, std::milli> diff_latency_mul = end_latency_mul - start_latency_mul;
-    std::cout << "Executed " << run_instructions_latency_mul << " in " << diff_latency_mul.count() << " milliseconds." << std::endl
-              << "Resulting in " << run_instructions_latency_mul / diff_latency_mul.count() * 1000 << " Instructions per Second!"
+    std::cout << "Executed " << run_instructions_latency_mul << " \"Mul\" Instructions in " << diff_latency_mul.count() << " milliseconds on a single Unit." << std::endl
+              << "Resulting in a Throughput of " << run_instructions_latency_mul / diff_latency_mul.count() * 1000 << " Instructions per Second!"
               << std::endl
               << std::endl;
 
